d871f657faac319a4c396116bf4eda0c58c89be5
allexer777aa-dev <allexer777.aa@gmail.com>
2026-07-31 23:20:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const card = document.querySelector(".card");
+
+    document.addEventListener("mousemove", (e) => {
+
+        const x = (e.clientX / window.innerWidth - 0.5) * 12;
+        const y = (e.clientY / window.innerHeight - 0.5) * 12;
+
+        card.style.transform = `
+            rotateY(${x}deg)
+            rotateX(${-y}deg)
+        `;
+
+    });
+
+    document.addEventListener("mouseleave", () => {
+        card.style.transform = "rotateX(0deg) rotateY(0deg)";
+    });
+
+});
